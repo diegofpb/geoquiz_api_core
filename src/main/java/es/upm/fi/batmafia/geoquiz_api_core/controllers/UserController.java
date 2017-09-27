46683b0c3e86139c6aa6f5d1ed57a1399a229206
Diego Fernández Peces-Barba <diegofpb93@gmail.com>
2017-09-27 21:15:47 +0200

    UserController created

---
+++ src/main/java/es/upm/fi/batmafia/geoquiz_api_core/controllers/UserController.java
@@ -0,0 +1,9 @@
+package es.upm.fi.batmafia.geoquiz_api_core.controllers;
+
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
+public class UserController {
+
+
+}
