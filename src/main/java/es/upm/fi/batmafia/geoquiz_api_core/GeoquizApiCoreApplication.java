68507c71f9d2ec07f38d6fd504437651ad8465b6
Diego Fernández Peces-Barba <diegofpb93@gmail.com>
2017-09-27 15:26:20 +0200

    Initialize project

---
+++ src/main/java/es/upm/fi/batmafia/geoquiz_api_core/GeoquizApiCoreApplication.java
@@ -0,0 +1,12 @@
+package es.upm.fi.batmafia.geoquiz_api_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoquizApiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoquizApiCoreApplication.class, args);
+	}
+}
