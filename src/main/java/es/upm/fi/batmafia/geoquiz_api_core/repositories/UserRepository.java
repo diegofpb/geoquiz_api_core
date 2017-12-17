fbdc25f30ab0ecaa2d77ee917319f6a24cedfb9e
Pablo Garcia <yinxro@gmail.com>
2017-12-17 11:52:54 +0100

    a

--- src/main/java/es/upm/fi/batmafia/geoquiz_api_core/repositories/UserRepository.java
+++ src/main/java/es/upm/fi/batmafia/geoquiz_api_core/repositories/UserRepository.java
@@ -4,9 +4,13 @@ import es.upm.fi.batmafia.geoquiz_api_core.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "user", path = "users")
 public interface UserRepository extends JpaRepository<User, String> {
 
     User findByUsername(String username);
+
+    List<User> findAll();
 
 }
